c8ad8907c2ce2d45d27670fd1639fa895b43e567
222774 <222774@edu.fa.ru>
2024-04-23 21:36:38 +0300

    final

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+import numpy as np
+from multiprocessing import Pool
 
+def multiply_elements(args):
+    i, j = args
+    result = A[i][j] * B[i][j]
+    # Записываем результат в промежуточный файл
+    with open('matrix_C.txt', 'a') as f:
+        f.write(str(result) + '\n')
+    return result
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+# Считываем матрицы из файлов
+A = np.loadtxt('matrix_A.txt')
+B = np.loadtxt('matrix_B.txt')
 
+# Проверяем, что матрицы имеют одинаковые размеры
+if A.shape != B.shape:
+    raise ValueError('Матрицы должны иметь одинаковые размеры')
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
+# Создаем пул процессов
+pool = Pool(processes=4)
 
-print(element((1, 0), matrix1, matrix2))
+# Вычисляем элементы матрицы-произведения в несколько потоков
+C = np.zeros(A.shape)
+for i in range(A.shape[0]):
+    for j in range(A.shape[1]):
+        C[i][j] = pool.apply_async(multiply_elements, args=((i, j),)).get()
+
+# Записываем матрицу-произведение в файл
+np.savetxt('matrix_C_final.txt', C)
